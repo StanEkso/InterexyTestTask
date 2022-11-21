22d295f6d330f813c1814d5caed557f8a3aeb7af
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-11-21 17:42:27 +0300

    fix: copy script

--- server/src/utils/copyAssets.ts
+++ server/src/utils/copyAssets.ts
@@ -1,2 +1,7 @@
 import shelljs from "shelljs";
+import fs from "fs";
 shelljs.cp("-r", "src/views", "dist");
+fs.readdir("dist/views", (err, files) => {
+  if (err) console.log(err);
+  console.log(files);
+});

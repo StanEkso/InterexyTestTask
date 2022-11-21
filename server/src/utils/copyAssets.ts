a35a1e741cf81eb4e614b197b65cf5ad1464370b
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-11-21 17:44:07 +0300

    fix: copy errors

--- server/src/utils/copyAssets.ts
+++ server/src/utils/copyAssets.ts
@@ -1,5 +1,9 @@
 import shelljs from "shelljs";
 import fs from "fs";
+fs.readdir("./", (err, files) => {
+  if (err) console.log(err);
+  console.log(files);
+});
 shelljs.cp("-r", "src/views", "dist");
 fs.readdir("dist/views", (err, files) => {
   if (err) console.log(err);
